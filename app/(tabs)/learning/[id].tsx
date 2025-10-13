c58aeec233fc4f1c1840900442b632051099b98d
muahe2004 <lyvanminh280504@gmail.com>
2025-10-13 22:53:25 +0700

    func check progress

--- app/(tabs)/learning/[id].tsx
+++ app/(tabs)/learning/[id].tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/Button/Button";
 import Header from "@/components/Header/Header";
+import { useUserInfo } from "@/hooks/useGetUserInfor";
 import { LearningNavigation } from "@/modules/course/components/LearningNavigation";
 import { QuestionBox } from "@/modules/course/components/QuestionBox";
 import { Lectures, ListQuestions } from "@/modules/course/types";
@@ -15,11 +16,14 @@ import { colors } from "../../../assets/styles/theme";
 export default function LearningScreen() {
     const { id } = useLocalSearchParams<{ id: string }>();
     const playerRef = useRef<any>(null);
+    const { user, loading } = useUserInfo();
 
     const [lecture, setLecture] = useState<Lectures | null>(null);
     const [questions, setQuestions] = useState<ListQuestions[]>([]);
     const [videoID, setVideoID] = useState("");
     const [duration, setDuration] = useState<number | null>(null);
+    const [openMenu, setOpenMenu] = useState(false);
+    const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
     const API_URL = process.env.EXPO_PUBLIC_UNILEARN_API;
 
@@ -46,34 +50,104 @@ export default function LearningScreen() {
 
     useEffect(() => {
         const fetchQuestionsAndAnswers = async () => {
-        try {
-            const qRes = await fetch(
-            `${API_URL}/questions?page=1&pageSize=100&search=&baiHocId=${id}`
-            );
-            const json = await qRes.json();
+            try {
+                const qRes = await fetch(`${API_URL}/questions?page=1&pageSize=100&search=&baiHocId=${id}`);
 
-            if (!qRes.ok) {
-            console.error("API error:", json);
-            setQuestions([]);
-            return;
+                const json = await qRes.json();
+
+                if (!qRes.ok) {
+                    console.error("API error:", json);
+                    setQuestions([]);
+                    return;
+                }
+
+                setQuestions(json.data);
+            } catch (err) {
+                console.error("Error fetching questions:", err);
+                setQuestions([]);
             }
-
-            setQuestions(json.data);
-        } catch (err) {
-            console.error("Error fetching questions:", err);
-            setQuestions([]);
-        }
         };
 
         fetchQuestionsAndAnswers();
     }, [id]);
 
-    const [openMenu, setOpenMenu] = useState(false);
+    const checkVideoProgress = () => {
+        if (!playerRef.current || !duration) return;
 
-    const handleMenu = () => {
-        setOpenMenu(true);  // mở menu
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+        }
+
+        const frequency = getFrequency(duration);
+
+        intervalRef.current = setInterval(async () => {
+            const current = await playerRef.current?.getCurrentTime();
+            if (!current || !duration) return;
+
+            const progress = (current / duration) * 100;
+
+            if (progress >= 5) {
+                clearInterval(intervalRef.current!);
+                // add progress
+                handleAddProgress();
+            }
+        }, frequency);
     };
 
+    const getFrequency = (duration: number | null | undefined) => {
+        if (duration === null || duration === undefined) return 10; 
+
+        if (duration < 300) {
+            return 10000; 
+        } else if (duration < 900) {
+            return 15000; 
+        } else if (duration < 1800) {
+            return 30000; 
+        } else {
+            return 60000; 
+        }
+    };
+
+    const handleMenu = () => {
+        setOpenMenu(true); 
+    };
+
+    const handlePlayVideo = (state: string) => {
+        if (state === "playing") {
+            checkVideoProgress();
+        }
+    };
+
+    const handleAddProgress = async () => {
+        try {
+            const userID = user?.id;
+            if (!userID || !id) { return;}
+
+            const res = await fetch(`${API_URL}/progress`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    nguoiDungId: userID,
+                    baiHocId: id,
+                    daHoanThanh: true,
+                }),
+            });
+
+            if (!res.ok) {
+                const error = await res.json();
+                console.error("Update progress failed!", error);
+                return;
+            }
+
+            const data = await res.json();
+            // console.log("Tiến độ đã được lưu:", data);
+
+        } catch (error) {
+            console.error("Lỗi khi cập nhật tiến độ học!");
+        }
+    }
 
     return (
         <SafeAreaView key={id} style={{ flex: 1, backgroundColor: "#fff", marginBottom: 80 }}>
@@ -86,30 +160,14 @@ export default function LearningScreen() {
                     <View style={styles.videoWrapper}>
                     {videoID ? (
                         <YoutubePlayer
-                        ref={playerRef}
-                        height={230}
-                        play={false}
-                        videoId={videoID}
-                        onChangeState={async (state: string) => {
-                            console.log("Trạng thái video:", state);
-                            if (state === "playing") {
-                            console.log("Người dùng đang xem video");
-                            }
-                            if (state === "paused") {
-                            console.log("Người dùng đã pause");
-                            }
+                            ref={playerRef}
+                            height={230}
+                            play={false}
+                            videoId={videoID}
+                            onChangeState={async (state: string) => { 
+                                handlePlayVideo(state);
+                            }}
 
-                            if (duration) {
-                            const current = await playerRef.current?.getCurrentTime();
-                            const progress = (current / duration) * 100;
-
-                            console.log(`Đã xem: ${progress.toFixed(2)}%`);
-
-                            if (progress >= 80) {
-                                console.log("✅ Người dùng đã hoàn thành video (>= 80%)");
-                            }
-                            }
-                        }}
                         />
                     ) : (
                         <Text>Không tìm thấy video</Text>
